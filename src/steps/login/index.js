62c251c2875bd53e7de68bc7690a7afe5a45e0b5
Ajit Bohra <ajit@lubus.in>
2024-09-10 00:16:04 +0530

    remove block save

--- src/steps/login/index.js
+++ src/steps/login/index.js
@@ -7,25 +7,14 @@ import { login } from '@wordpress/icons';
 /**
  * Internal dependencies.
  */
-import Edit from './edit';
-import save from './save';
 import metadata from './block.json';
+import Edit from './edit';
 import './style.scss';
 
 /**
  * Every block starts by registering a new block type definition.
  */
 registerBlockType(metadata.name, {
-
 	icon: login,
-
-	/**
-	 * @see ./edit.js
-	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
-	save,
 });
