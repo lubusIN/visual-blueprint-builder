9376f05f64b922fef6839d37f24e645071acc860
Ajit Bohra <ajit@lubus.in>
2024-09-10 09:50:02 +0530

    code cleanup

--- src/steps/login/index.js
+++ src/steps/login/index.js
@@ -8,24 +8,12 @@ import { login } from '@wordpress/icons';
  * Internal dependencies.
  */
 import Edit from './edit';
-import save from './save';
 import metadata from './block.json';
-import './style.scss';
 
 /**
  * Every block starts by registering a new block type definition.
  */
 registerBlockType(metadata.name, {
-
 	icon: login,
-
-	/**
-	 * @see ./edit.js
-	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
-	save,
 });
