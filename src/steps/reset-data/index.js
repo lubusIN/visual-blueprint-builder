cc1c4f7e2d47d09fc4c4bb3e4ad7c3a7620cfa09
Ajit Bohra <ajit@lubus.in>
2024-09-10 21:44:09 +0530

    add reset data

---
+++ src/steps/reset-data/index.js
@@ -0,0 +1,19 @@
+/**
+ * Wordpress dependencies.
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { reusableBlock } from '@wordpress/icons';
+
+/**
+ * Internal dependencies.
+ */
+import metadata from './block.json';
+import Edit from './edit';
+
+/**
+ * Every block starts by registering a new block type definition.
+ */
+registerBlockType(metadata.name, {
+	icon: reusableBlock,
+	edit: Edit,
+});
