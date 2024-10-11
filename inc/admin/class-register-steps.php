994e5948b909befa9eff06849b80141805e228fb
Punit Verma <79571365+Verma-Punit@users.noreply.github.com>
2024-10-11 14:44:07 +0530

    restrict blueprint steps to blueprint post type (#37)

--- inc/admin/class-register-steps.php
+++ inc/admin/class-register-steps.php
@@ -11,7 +11,7 @@ namespace Lubus\Admin\BlockEditorForPlaygroundBlueprint;
 class BlueprintSteps
 {
     /**
-     * Construct that hooks into WordPress to initialize a blueprint steps.
+     * Construct that hooks into WordPress to initialize blueprint steps.
      */
     public function __construct()
     {
@@ -23,30 +23,35 @@ class BlueprintSteps
      */
     public function register_blueprint_steps()
     {
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/login');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/install-plugin'); 
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/enable-multisite'); 
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/copy-file');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/install-plugin');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/install-theme');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/define-site-url');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/activate-theme');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/activate-plugin');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/import-wordpress-files');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/remove-dir');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/remove-file');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/reset-data');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/move');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/define-wp-config-consts');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/write-file');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/wp-cli');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/run-php');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/make-dir');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/import-wxr');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/update-user-meta');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/unzip');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/set-site-options');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/set-site-language');
-        register_block_type(BEPB_PLUGIN_DIR . 'build/steps/import-theme-starter-content');
+        $blueprint_steps = [
+            'login',
+            'install-plugin',
+            'enable-multisite',
+            'copy-file',
+            'install-theme',
+            'define-site-url',
+            'activate-theme',
+            'activate-plugin',
+            'import-wordpress-files',
+            'remove-dir',
+            'remove-file',
+            'reset-data',
+            'move',
+            'define-wp-config-consts',
+            'write-file',
+            'wp-cli',
+            'run-php',
+            'make-dir',
+            'import-wxr',
+            'update-user-meta',
+            'unzip',
+            'set-site-options',
+            'set-site-language',
+            'import-theme-starter-content',
+        ];
+
+        foreach ($blueprint_steps as $step) {
+            register_block_type(BEPB_PLUGIN_DIR . 'build/steps/' . $step);
+        }
     }
 }
