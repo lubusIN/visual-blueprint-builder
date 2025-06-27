caefaebf58c34270202a1841bb9057af7e7b22a7
GitHub Action <action@github.com>
2025-06-27 08:05:45 +0000

    Build and deploy to playground

---
+++ build/steps/import-theme-starter-content/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c07eecd083bab99cc11b');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp/playground-blueprint-editor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '337dafaac42be20fabaa51569be3e47c1db08fc1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wp/playground-blueprint-editor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '337dafaac42be20fabaa51569be3e47c1db08fc1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
